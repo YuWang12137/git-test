0d72eda96cbee81a00ce15b2d239431c91a04c0d
geigei <1789186483@qq.com>
2022-05-06 19:31:27 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 王建宇
+ * @create 2022-05-06 19:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
